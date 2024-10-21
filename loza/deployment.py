9c99e6046a35e97b4df7d8e0067fa4e1066bf553
Skafil <filipsuska20@gmail.com>
2024-10-21 16:05:15 +0200

    zmiana allowedhost i csrft token

--- loza/deployment.py
+++ loza/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = ['lozaszydercow3-dxh2b7gdb4a4bfat.northeurope-01.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://lozaszydercow3-dxh2b7gdb4a4bfat.northeurope-01.azurewebsites.net']
 DEBUG = True
 
 MIDDLEWARE = [
@@ -25,7 +25,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+parameters = {pair.split('='):pair.split('=')[1] for pair i connection_string.split(' ')}
 
 
 DATABASES = {
