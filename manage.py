e83908abee8debb79d8121f88230a18304b733de
Skafil <filipsuska20@gmail.com>
2024-10-22 16:52:06 +0200

    Poprawki do manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,15 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loza.settings')
+    
+    if 'WEBSITE_HOSTNAME' not in os.environ:
+        print("Loading environment variables for .env file")
+        load_dotenv('./.env')
+    
+    settings_module = "loza.production" if 'WEBSITE_HOSTNAME' in os.environ else 'loza.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
