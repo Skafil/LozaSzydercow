8b93f295d4aae787d7d3fdb04e7fa9f82949eca8
Skafil <filipsuska20@gmail.com>
2024-10-23 20:55:31 +0200

    Wylacz DEBUG, popraw strone home

--- loza/production.py
+++ loza/production.py
@@ -6,7 +6,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = True
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
