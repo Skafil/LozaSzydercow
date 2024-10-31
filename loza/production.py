6b668ad00eeed077727c3d6a887abe85badea49b
Skafil <filipsuska20@gmail.com>
2024-10-31 20:20:05 +0100

    Debug False --> True

--- loza/production.py
+++ loza/production.py
@@ -6,7 +6,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], 'lozaszydercow4.pl'] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME'], 'lozaszydercow4.pl'] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = False
+DEBUG = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
